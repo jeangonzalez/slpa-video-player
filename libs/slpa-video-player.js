7eeff4e99a690cda16e105e34e95ce011bb3b608
Jean Gonzalez <jean.gonzalez@sillpacorp.com>
2016-02-23 09:36:27 -0500

    Update 2016-02-23T14:34:46.006Z

--- libs/slpa-video-player.js
+++ libs/slpa-video-player.js
@@ -220,23 +220,6 @@ angular.module('slpaVideoPlayer.directives', [])
 /**
  * Created by JG on 2/22/2016.
  */
-angular.module('slpaVideoPlayer.filters',[])
-  .filter('secondsToHHmmss', function($filter) {
-    return function(seconds) {
-      return $filter('date')(new Date(0, 0, 0).setSeconds(seconds), 'HH:mm:ss');
-    };
-  })
-  .filter('trustUrl', ['$sce', function ($sce) {
-    return function (recordingUrl) {
-      return $sce.trustAsResourceUrl(recordingUrl);
-    };
-  }]);
-
-
-
-/**
- * Created by JG on 2/22/2016.
- */
 angular.module('slpaVideoPlayer.services', [])
   .factory('localData', function($localStorage) {
     //
@@ -258,3 +241,20 @@ angular.module('slpaVideoPlayer.services', [])
     //  }
     //};
   });
+
+/**
+ * Created by JG on 2/22/2016.
+ */
+angular.module('slpaVideoPlayer.filters',[])
+  .filter('secondsToHHmmss', function($filter) {
+    return function(seconds) {
+      return $filter('date')(new Date(0, 0, 0).setSeconds(seconds), 'HH:mm:ss');
+    };
+  })
+  .filter('trustUrl', ['$sce', function ($sce) {
+    return function (recordingUrl) {
+      return $sce.trustAsResourceUrl(recordingUrl);
+    };
+  }]);
+
+
